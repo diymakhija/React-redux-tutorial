0291a58fe34fb0cde4326a9ee13cbeb22583c536
DIY Makhija <diymakhija@gmail.com>
2023-04-25 13:39:17 -0700

    Add basic Redux example

---
+++ redux-demo/index.js
@@ -0,0 +1,62 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+console.log("From index.js");
+
+// Action name 
+const CAKE_ORDERED = 'CAKE_ORDERED';
+
+// Action Creator 
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1
+  }
+}
+
+// State of the application can be maintaned as single object
+const initialState = {
+  numOfCakes: 10
+}
+
+
+// Reducer 
+// (previousState action) => newState
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      }
+    default:
+      return state;
+  }
+}
+
+/**
+ * Responsibilities of store 
+ * 
+ * 1. Holds application state 
+ * 2. Allows access to state via getState()
+ * 3. Allows state to be updated via dispatch(action)
+ * 4. Registers listener via subscribe(listener)
+ * 5. Hanldes unregistering of listeners via the function returned by subscribe(listener)
+ * 
+ */
+
+const store = createStore(reducer); // Responsibility 1 - reducer manages state and get initialState
+console.log('Initial state ', store.getState());   // Responsibility 2
+
+const unsubscribe = store.subscribe(() => { // Responsibility 4
+  console.log('Update state ', store.getState());
+});
+
+store.dispatch(orderCake()); // Responsibility 3
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe(); // Responsibility 5
+
+
