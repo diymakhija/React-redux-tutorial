c60a26e7d806eb51978dd1bb48efec43ecc21217
DIY Makhija <diymakhija@gmail.com>
2023-05-01 04:56:47 -0700

    Integrate redux store with views and dispatch actions

--- react-redux-toolkit-demo/src/features/icecream/IcecreamView.jsx
+++ react-redux-toolkit-demo/src/features/icecream/IcecreamView.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "./icecreamSlice";
 
 const IcecreamView = () => {
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+  const dispatch = useDispatch();
   return (
     <div>
-      <h2>Number of icecreams - </h2>
-      <button>Order ice cream</button>
-      <button>Restock ice creams</button>
+      <h2>Number of icecreams - {numOfIcecreams}</h2>
+      <button onClick={() => dispatch(ordered())}>Order ice cream</button>
+      <button onClick={() => dispatch(restocked(5))}>Restock ice creams</button>
     </div>
   );
 };
