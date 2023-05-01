7b7202b3f6c5fb4c4b5812806108e494a9031e02
DIY Makhija <diymakhija@gmail.com>
2023-05-01 05:03:53 -0700

    Add input field to take icecream restock value

--- react-redux-toolkit-demo/src/features/icecream/IcecreamView.jsx
+++ react-redux-toolkit-demo/src/features/icecream/IcecreamView.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { ordered, restocked } from "./icecreamSlice";
 
 const IcecreamView = () => {
+  const [value, setValue] = useState(1);
   const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
   const dispatch = useDispatch();
   return (
     <div>
       <h2>Number of icecreams - {numOfIcecreams}</h2>
       <button onClick={() => dispatch(ordered())}>Order ice cream</button>
-      <button onClick={() => dispatch(restocked(5))}>Restock ice creams</button>
+      <input type="number"  value={value} onChange={(event) => setValue(parseInt(event.target.value))}/>
+      <button onClick={() => dispatch(restocked(value))}>Restock ice creams</button>
     </div>
   );
 };
