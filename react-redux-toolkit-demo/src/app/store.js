7411635a1d6b1add90950dbfa86281cecf697c1f
DIY Makhija <diymakhija@gmail.com>
2023-05-01 04:43:59 -0700

    Add react-redux librray and configure store through Provider

--- react-redux-toolkit-demo/src/app/store.js
+++ react-redux-toolkit-demo/src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reduxLogger from "redux-logger";
+// import reduxLogger from "redux-logger";
 import cakeReducer from "../features/cake/cakeSlice";
 import icecreamReducer from "../features/icecream/icecreamSlice";
 import userReducer from "../features/user/userSlice";
 
-const logger = reduxLogger.createLogger();
+// const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
@@ -12,7 +12,7 @@ const store = configureStore({
     icecream: icecreamReducer,
     user: userReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
