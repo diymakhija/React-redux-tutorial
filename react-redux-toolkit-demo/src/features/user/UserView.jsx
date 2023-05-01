6d260426e0740897523ef9af08e24736ac05a22f
DIY Makhija <diymakhija@gmail.com>
2023-05-01 05:35:31 -0700

    Fetch list of users from an API and display in view

--- react-redux-toolkit-demo/src/features/user/UserView.jsx
+++ react-redux-toolkit-demo/src/features/user/UserView.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "./userSlice";
 
 const UserView = () => {
-  return <h2>List of Users</h2>;
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>List of Users </h2>
+      {user.loading && <div> Loading ...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.data.length ? (
+        <ul>
+          {user.data.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
 };
 
 export default UserView;
