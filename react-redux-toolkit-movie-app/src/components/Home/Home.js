1ed9026b07c5828317c60b931c038e6c6353ee54
DIY Makhija <diymakhija@gmail.com>
2023-05-03 03:23:09 -0700

    Fetch movies and shows details in Home page

--- react-redux-toolkit-movie-app/src/components/Home/Home.js
+++ react-redux-toolkit-movie-app/src/components/Home/Home.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieListing from '../MovieListing/MovieListing';
+import { useDispatch  } from 'react-redux';
+import { fetchAsyncMovies, fetchAsyncShows } from '../../features/movies/movieSlice';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch])
   return (
     <div>
-      Home Page
+      <div className="banner-img"></div>
+      <MovieListing />
     </div>
   );
 };
